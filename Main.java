bb68665521d5f639bb083639e18b2162c6d55028
Arthur <40115007+Arthur100024@users.noreply.github.com>
2021-09-04 15:08:39 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,23 @@
+import java.lang.*;
+public class Main
+{
+
+  public static void main (String[]args)
+  {
+
+    doggy d1 = new doggy ("Mike", 2);
+
+    book b2 = new book ();
+
+    ball ba3 = new ball ("blue");
+
+      ba3.setRadius (18);
+
+      d1.vyvod ();
+
+      b2.vyvod ();
+
+      ba3.vyvod ();
+
+  }
+}
